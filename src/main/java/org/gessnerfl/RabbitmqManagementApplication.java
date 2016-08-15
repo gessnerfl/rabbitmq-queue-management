bf33a557b07e78d04ac4516bb7f2e45d7225d28e
Florian Gessner <flo.gessner@gmail.com>
2016-08-15 21:07:03 +0200

    Iniital commit

---
+++ src/main/java/org/gessnerfl/RabbitmqManagementApplication.java
@@ -0,0 +1,12 @@
+package org.gessnerfl;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RabbitmqManagementApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(RabbitmqManagementApplication.class, args);
+	}
+}
