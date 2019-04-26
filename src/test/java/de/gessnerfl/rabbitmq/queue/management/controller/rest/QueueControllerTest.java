5f159528fe0ea667e8047af509435201d762c675
Florian Gessner <florian.gessner@tis.biz>
2019-04-26 11:10:37 +0200

    #10 update third party libraries

--- src/test/java/de/gessnerfl/rabbitmq/queue/management/controller/rest/QueueControllerTest.java
+++ src/test/java/de/gessnerfl/rabbitmq/queue/management/controller/rest/QueueControllerTest.java
@@ -12,7 +12,7 @@ import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.mockito.InjectMocks;
 import org.mockito.Mock;
-import org.mockito.runners.MockitoJUnitRunner;
+import org.mockito.junit.MockitoJUnitRunner;
 
 import de.gessnerfl.rabbitmq.queue.management.model.Message;
 import de.gessnerfl.rabbitmq.queue.management.model.remoteapi.Queue;
--- src/test/java/de/gessnerfl/rabbitmq/queue/management/service/rabbitmq/remoteapi/ManagementApiFactoryTest.java
+++ src/test/java/de/gessnerfl/rabbitmq/queue/management/service/rabbitmq/remoteapi/ManagementApiFactoryTest.java
@@ -8,7 +8,7 @@ import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.mockito.InjectMocks;
 import org.mockito.Mock;
-import org.mockito.runners.MockitoJUnitRunner;
+import org.mockito.junit.MockitoJUnitRunner;
 
 import de.gessnerfl.rabbitmq.queue.management.service.rabbitmq.remoteapi.ManagementApi;
 import de.gessnerfl.rabbitmq.queue.management.service.rabbitmq.remoteapi.ManagementApiUrlBuilder;
