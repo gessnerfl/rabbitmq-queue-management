d118e125ec021aacc2a416a7a5c86c1ac83375c6
Florian Gessner <flo.gessner@gmail.com>
2019-04-27 22:52:48 +0200

    fix deletion of spring boot application launcher

---
+++ src/main/java/de/gessnerfl/rabbitmq/queue/management/RabbitmqQueueManagementApplication.java
@@ -0,0 +1,13 @@
+package de.gessnerfl.rabbitmq.queue.management;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RabbitmqQueueManagementApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(RabbitmqQueueManagementApplication.class, args);
+    }
+
+}
