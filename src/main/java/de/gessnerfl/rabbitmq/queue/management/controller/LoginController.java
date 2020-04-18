5905e5b68908a5fbf2d4f000da9054b63f8945af
Florian Gessner <flo.gessner@gmail.com>
2020-04-18 21:17:36 +0200

    #13: fix broken build; remove unneeded import

--- src/main/java/de/gessnerfl/rabbitmq/queue/management/controller/LoginController.java
+++ src/main/java/de/gessnerfl/rabbitmq/queue/management/controller/LoginController.java
@@ -8,7 +8,6 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
-import sun.jvm.hotspot.debugger.Page;
 
 import javax.servlet.http.HttpServletRequest;
 
