eea344a4d4fe507d2778f97055a69e3bc9303da6
Florian Gessner <flo.gessner@gmail.com>
2022-02-19 21:52:26 +0100

    Update dependencies and remove unused import

--- src/main/java/de/gessnerfl/rabbitmq/queue/management/controller/Pages.java
+++ src/main/java/de/gessnerfl/rabbitmq/queue/management/controller/Pages.java
@@ -1,13 +1,12 @@
 package de.gessnerfl.rabbitmq.queue.management.controller;
 
-import org.springframework.web.servlet.mvc.support.RedirectAttributes;
-
 public enum Pages {
 
     INDEX("/index"),
     MESSAGES("/messages");
 
     private String path;
+
     private Pages(String path){
         this.path=path;
     }
