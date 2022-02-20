da0b0a4b5778a8ba2fbe5dd75b6376e47eece9c8
Nejc Tomažič <nejc.tomazic@global.id>
2022-02-20 12:08:03 +0100

    add ssl connection support

--- src/main/java/de/gessnerfl/rabbitmq/queue/management/connection/ConnectionFactories.java
+++ src/main/java/de/gessnerfl/rabbitmq/queue/management/connection/ConnectionFactories.java
@@ -1,36 +1,46 @@
 package de.gessnerfl.rabbitmq.queue.management.connection;
 
+import java.security.KeyManagementException;
+import java.security.NoSuchAlgorithmException;
 import java.util.concurrent.ConcurrentHashMap;
 import java.util.concurrent.ConcurrentMap;
 
-import de.gessnerfl.rabbitmq.queue.management.service.rabbitmq.config.RabbitMqConfig;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 import com.rabbitmq.client.ConnectionFactory;
+
+import de.gessnerfl.rabbitmq.queue.management.service.rabbitmq.config.RabbitMqConfig;
 
 @Service
 public class ConnectionFactories {
     private final RabbitMqConfig configuration;
 
     private final ConcurrentMap<String, ConnectionFactory> connectionsByVirtualHost = new ConcurrentHashMap<>();
-    
+
     @Autowired
     public ConnectionFactories(RabbitMqConfig configuration) {
         this.configuration = configuration;
     }
-    
-    public ConnectionFactory getOrCreate(String vhost){
+
+    public ConnectionFactory getOrCreate(String vhost) {
         return connectionsByVirtualHost.computeIfAbsent(vhost, this::createFor);
     }
-    
-    private ConnectionFactory createFor(String vhost){
+
+    private ConnectionFactory createFor(String vhost) {
         ConnectionFactory connectionFactory = new ConnectionFactory();
         connectionFactory.setHost(configuration.getHostname());
         connectionFactory.setPort(configuration.getPort());
         connectionFactory.setVirtualHost(vhost);
         connectionFactory.setUsername(configuration.getUsername());
         connectionFactory.setPassword(configuration.getPassword());
+        if (configuration.useSsl()) {
+            try {
+                connectionFactory.useSslProtocol();
+            } catch (NoSuchAlgorithmException | KeyManagementException e) {
+                throw new ConnectionFailedException(e);
+            }
+        }
         return connectionFactory;
     }
 }
--- src/main/java/de/gessnerfl/rabbitmq/queue/management/service/rabbitmq/config/RabbitMqConfig.java
+++ src/main/java/de/gessnerfl/rabbitmq/queue/management/service/rabbitmq/config/RabbitMqConfig.java
@@ -3,12 +3,12 @@ package de.gessnerfl.rabbitmq.queue.management.service.rabbitmq.config;
 import org.springframework.boot.context.properties.ConfigurationProperties;
 import org.springframework.stereotype.Component;
 
-
 @Component
 @ConfigurationProperties(prefix = "de.gessnerfl.rabbitmq")
 public class RabbitMqConfig {
 
     private String hostname;
+    private boolean useSsl = false;
     private int port = 5672;
     private int managementPort = 15672;
     private boolean managemnetPortSecured = false;
@@ -21,6 +21,14 @@ public class RabbitMqConfig {
 
     public void setHostname(String hostname) {
         this.hostname = hostname;
+    }
+
+    public boolean useSsl() {
+        return useSsl;
+    }
+
+    public void setUseSsl(boolean useSsl) {
+        this.useSsl = useSsl;
     }
 
     public int getPort() {
